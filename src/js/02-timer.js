3a19f15f42138bbc5f6f574bc58a3dc294fd531b
Iryna Artymuk <artimyk@ukr.net>
2022-12-29 15:27:12 +0200

    task 2 done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,8 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-const body = document.querySelector(' body')
-const inputWrap = document.querySelector('.input-wrap')
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const body = document.querySelector(' body');
+const inputWrap = document.querySelector('.input-wrap');
 
 const timer = document.querySelector('.timer');
 const days = document.querySelector('[data-days]');
@@ -10,26 +11,22 @@ const min = document.querySelector('[data-minutes]');
 const sec = document.querySelector('[ data-seconds]');
 const start = document.querySelector('[ data-start]');
 let timerId = null;
-start.addEventListener('click',countLeftTime)
+let deltaTime =null
+start.addEventListener('click', countLeftTime);
+let targetDate = null;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    targetDate = selectedDates[0].getTime();
+  
+  },
+};
 
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     console.log(choosenDate.selectedDates[0])
-//   },
-// };
+flatpickr('#datetime-picker', options);
 
-// const choosenDate = flatpickr('#datetime-picker', options);
-
-
-// const targetDate = choosenDate.selectedDates[0].getTime();
-// console.dir(choosenDate)
-
-// console.log(targetDate)
-    const targetDate = new Date('2022-12-31 23:59:59').getTime();
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -56,26 +53,28 @@ function updateTextContent(time) {
   sec.textContent = time.seconds;
 }
 
-
 function countLeftTime() {
-  body.style.backgroundColor = ' #000'
-  timer.style.visibility='visible';
-  inputWrap.innerHTML=""
- timerId= setInterval(() => {
+  body.style.backgroundColor = ' #000';
+  timer.style.visibility = 'visible';
+  inputWrap.innerHTML = '';
 
+     timerId = setInterval(() => {
 
-const currentDate = Date.now()
+    const currentDate = Date.now();
+   
+     deltaTime = targetDate - currentDate;
+     if (deltaTime < 0) {
 
-const deltaTime = targetDate - currentDate;
-
-const countTime= convertMs(deltaTime )
-    if (deltaTime < 0) {
-      clearInterval(timerId)
+      Notify.failure('Please, pick  the date in the future', {
+        width: '520px', height: '520px',
+        borderRadius: '8px', position: 'center-top',
+      })
       timer.innerHTML = " TIME EXPIRED";
+      clearInterval( timerId)
+      return 
+    }
+    const countTime = convertMs(deltaTime);
+   
+    updateTextContent(countTime);
+  }, 1000);
 }
-
-  updateTextContent(countTime);
-  },1000)
-
-}
-
